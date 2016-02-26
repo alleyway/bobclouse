e5d9da05a430cb5b86f0afa89e40296aa99b3ec0
Michael Lake <mlake900@gmail.com>
2016-02-26 13:48:20 -0500

    Adding paypal buttons

--- js/grayscale.js
+++ js/grayscale.js
@@ -42,7 +42,7 @@ $(function() {
 
 $(function() {
 
-    $("input,textarea").jqBootstrapValidation({
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
